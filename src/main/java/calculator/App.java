09b5947c6170297d8a8ddfa2a640d6c6a689a6ab
kiseokkm <311kskim@gmail.com>
2024-04-30 15:46:21 +0900

    level 1-6

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -49,20 +49,37 @@ public class App {
 
             if (realOperation) {
                 System.out.println("결과: " + result);
-                // 결과를 배열에 저장
                 if (index < 10) {
                     results[index] = result;
                     index++; // 인덱스 증가
-                    System.out.println("현재 저장된 결과의 수: " + index); //계산할때마다 몇개저장된지 몰라서 넣음
                 } else {
-                    System.out.println("10개초과");
+                    // 한칸씩 이동
+                    for (int i = 1; i < 10; i++) {
+                        results[i - 1] = results[i];
+                    }
+                    results[9] = result;
                 }
+                System.out.println("현재 저장된 결과의 수: " + index);
             }
+            System.out.print("현재 저장된 결과의 수: " + index);
+            System.out.print(" (");
+            for (int i = 0; i < index; i++) {
+                System.out.print(results[i] + (i < index - 1 ? ", " : "")); // 옮겨지나 확인
+            }
+            System.out.println(")");
 
-            // 계산 계속 여부 확인
-            System.out.println("더 계산하시겠습니까? ('go' 또는 '해'를 입력하면 계속합니다, 그 외는 종료)");
+            // 더 계산 하려면 'go' '해' 입력 
+            /*System.out.println("더 계산하시겠습니까? ('go' 또는 '해'를 입력하면 계속합니다, 그 외는 종료)");
             String input = sc.next();
             if (!input.equals("go") && !input.equals("해")) {
+                break;
+            }
+             */
+
+            // exit 입력하면 멈추기
+            System.out.println("계속하시려면 아무 키나 입력하세요 ('exit'를 입력하면 종료합니다)");
+            String input = sc.next();
+            if ( input.equalsIgnoreCase("exit") ||  input.equalsIgnoreCase("ㄷ턋")) {
                 break;
             }
         }
