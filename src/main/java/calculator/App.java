995486dbb790b626a2a7010c8fc80ea725c5f063
kiseokkm <311kskim@gmail.com>
2024-04-30 12:25:00 +0900

    level 1-2

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -12,6 +12,9 @@ public class App {
         System.out.print("두 번째 숫자를 입력하세요: ");
         int secondNumber = sc.nextInt();
         // Scanner를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장합니다.
-        System.out.println("첫번째숫자,두번째숫자="+firstNumber+","+secondNumber);
+
+        System.out.print("사칙연산 기호를 입력하세요: ");
+        char operator = sc.next().charAt(0);
+        System.out.println("operator = " + operator);
     }
 }
