27f464f8fa503d6c102580f59ac222fa199d2d9d
kiseokkm <311kskim@gmail.com>
2024-04-30 13:10:14 +0900

    level 1-5

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -6,7 +6,11 @@ public class App {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        while (true) {//while 문 반복
+        // 배열생성
+        int[] results = new int[10];
+        int index = 0; // 초기화
+
+        while (true) {
             System.out.print("첫 번째 숫자를 입력하세요: ");
             int firstNumber = sc.nextInt();
 
@@ -15,7 +19,6 @@ public class App {
 
             System.out.print("두 번째 숫자를 입력하세요: ");
             int secondNumber = sc.nextInt();
-
 
             int result = 0;
             boolean realOperation = true;
@@ -33,26 +36,40 @@ public class App {
                 case '/':
                     if (secondNumber == 0) {
                         System.out.println("다시 입력하세요. 나누기에서 두번째 수는 0이 될 수 없습니다.");
-                        realOperation = false; // 나눗셈 실패 시 끝
+                        realOperation = false;
                     } else {
                         result = firstNumber / secondNumber;
                     }
                     break;
                 default:
                     System.out.println("사칙연산 기호를 다시 입력하세요.");
-                    realOperation = false; // 이상한거 넣을시 끝
+                    realOperation = false;
+                    System.exit(0);
             }
 
             if (realOperation) {
                 System.out.println("결과: " + result);
+                // 결과를 배열에 저장
+                if (index < 10) {
+                    results[index] = result;
+                    index++; // 인덱스 증가
+                    System.out.println("현재 저장된 결과의 수: " + index); //계산할때마다 몇개저장된지 몰라서 넣음
+                } else {
+                    System.out.println("10개초과");
+                }
             }
 
-            // exit 입력시 종료 되신 go or 해 입력시 계속 , 그 외 종료
+            // 계산 계속 여부 확인
             System.out.println("더 계산하시겠습니까? ('go' 또는 '해'를 입력하면 계속합니다, 그 외는 종료)");
             String input = sc.next();
             if (!input.equals("go") && !input.equals("해")) {
                 break;
             }
+        }
+        // 배열에 저장된 모든 결과 출력
+        System.out.println("저장된 결과:");
+        for (int i = 0; i < index; i++) {
+            System.out.println("결과 " + (i + 1) + ": " + results[i]);
         }
 
         sc.close();
