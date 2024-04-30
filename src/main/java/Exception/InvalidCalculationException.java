7c24ae3e5c9be07fb617e3b1b0fbae7899e0fe49
kiseokkm <311kskim@gmail.com>
2024-04-30 16:31:36 +0900

    level 2-1

---
+++ src/main/java/Exception/InvalidCalculationException.java
@@ -0,0 +1,7 @@
+package main.java.Exception;
+
+public class InvalidCalculationException extends Exception {
+    public InvalidCalculationException(String message) {
+        super(message);
+    }
+}
--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,5 +1,42 @@
-package calculator;
+package main.java.calculator;
+
+import main.java.Exception.InvalidCalculationException;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Calculator {
+    private List<Integer> results; // 연산 결과를 저장하는 리스트
 
+    public Calculator() {
+        results = new ArrayList<>();
+    }
+    // 사칙연산 메서드
+    public int calculate(int firstNumber, int secondNumber, char operator) throws InvalidCalculationException {
+        int result = 0;
+        switch (operator) {
+            case '+':
+                result = firstNumber + secondNumber;
+                break;
+            case '-':
+                result = firstNumber - secondNumber;
+                break;
+            case '*':
+                result = firstNumber * secondNumber;
+                break;
+            case '/':
+                if (secondNumber == 0) {
+                    throw new InvalidCalculationException("계산불가");
+                }
+                result = firstNumber / secondNumber;
+                break;
+            default:
+                throw new InvalidCalculationException("연산기호가 잘못 됐습니다");
+        }
+        results.add(result);
+        return result;
+    }
+    public List<Integer> getResults() {
+        return results;
+    }
 }
