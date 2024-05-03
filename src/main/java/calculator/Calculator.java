396928167ade6835985519287fb8f987077307f7
kiseokkm <311kskim@gmail.com>
2024-05-03 10:40:57 +0900

    refactor

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -9,23 +9,19 @@ public class Calculator {
     private List<Double> results;  // Changed from List<Integer> to accommodate both integers and floating-point results
     private List<Double> circleAreas;
 
-    // Updated constructor to handle the new List<Double> for results
     public Calculator() {
         this.results = new ArrayList<>();
         this.circleAreas = new ArrayList<>();
     }
 
-    // Adds a result to the results list
     public void addResult(double result) {
         results.add(result);
     }
 
-    // Gets a copy of the results list
     public List<Double> getResults() {
         return new ArrayList<>(results);
     }
 
-    // Sets a new list of results
     public void setResults(List<Double> newResults) {
         this.results = new ArrayList<>(newResults);
     }
@@ -37,7 +33,6 @@ public class Calculator {
         }
     }
 
-    // Prints all stored results
     public void inquiryResults() {
         if (!results.isEmpty()) {
             System.out.println("저장된 모든 결과:");
@@ -49,22 +44,18 @@ public class Calculator {
         }
     }
 
-    // Adds a circle area to the circleAreas list
     public void addCircleArea(double area) {
         circleAreas.add(area);
     }
 
-    // Gets a copy of the circleAreas list
     public List<Double> getCircleAreas() {
         return new ArrayList<>(circleAreas);
     }
 
-    // Sets a new list of circle areas
     public void setCircleAreas(List<Double> newCircleAreas) {
         this.circleAreas = new ArrayList<>(newCircleAreas);
     }
 
-    // Prints all stored circle areas
     public void inquiryCircleResults() {
         if (!circleAreas.isEmpty()) {
             System.out.println("저장된 원의 넓이 결과:");
